cdebd3fb96ec0ee4086808031deb98b2eb516df7
Syed Tamzid <tamzid2004@gmail.com>
2022-12-21 19:41:00 -0500

    Fix the mount command in ec2 script

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -3,5 +3,5 @@
 sudo mkdir /instance-vol
 sudo mkdir /etc/ecs
 sudo mkfs -t xfs /dev/nvme1n1
-sudo /dev/nvme1n1 /instance-vol
+sudo mount /dev/nvme1n1 /instance-vol
 sudo echo ECS_CLUSTER=ecs-cluster-for-unstructured >> /etc/ecs/ecs.config
