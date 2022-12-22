336344c3175b7ec4d47c3220335f57187d9a1b54
Syed Tamzid <tamzid2004@gmail.com>
2022-12-21 19:48:58 -0500

    Change the filesystem

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -2,6 +2,6 @@
 
 sudo mkdir /instance-vol
 sudo mkdir /etc/ecs
-sudo mkfs -t xfs /dev/nvme1n1
+sudo mkfs.ext4 /dev/nvme1n1
 sudo mount /dev/nvme1n1 /instance-vol
 sudo echo ECS_CLUSTER=ecs-cluster-for-unstructured >> /etc/ecs/ecs.config
